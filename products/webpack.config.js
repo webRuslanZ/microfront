a734bc3d8a3b7c0c7fedd3d0f33dfbada0f0b936
Your Name <rv.zamilov@gmail.com>
2024-04-09 10:13:30 +0300

    end of this app

--- products/webpack.config.js
+++ products/webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     },
     plugins: [
         new ModuleFederationPlugin({
-            name: 'products',
+            name: 'products', //название переменной в remoteEntry.js
             filename: 'remoteEntry.js',
             exposes: {
                 './ProductsIndex': './src/bootstrap.js'
